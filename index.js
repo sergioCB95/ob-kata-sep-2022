559d4f9cc75ac95a6c4820f78aee562a8413be6e
Fernando de la Torre <fernandodelatorre4@gmail.com>
2022-09-29 15:51:00 +0200

    feat: add api get

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const fs = require('fs');
+const fetch = require('node-fetch');
 
 const getInputAsString = (inputFile) => {
   const fileBuffer = fs.readFileSync(inputFile);
   return fileBuffer.toString();
 };
 
-const main = (inputFile = './input.txt') => {
-  const input = getInputAsString(inputFile);
-  console.log(`Input content => ${input}`);
+const getUrl = async (url) => {
+  const res = await fetch(url);
+  return res.json();
+};
+
+const main = async (inputFile = './input.txt', url = 'https://dummyjson.com/products') => {
+  try {
+    // const input = getInputAsString(inputFile);
+    // console.log(`Input content => ${input}`);
+
+    // const apiResponse = await getUrl(url);
+    // console.log(apiResponse);
+
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 main();
